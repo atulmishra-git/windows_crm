03c9002cacf7e726174f1aec250a68d2dc593803
annshress <ann.shress@gmail.com>
2020-03-28 21:43:01 +0545

    fix: login form

--- mainapp/forms/login.py
+++ mainapp/forms/login.py
@@ -1,4 +1,7 @@
 from django import forms
+from django.core.exceptions import ValidationError
+
+from mainapp.models import User
 
 
 class LoginForm(forms.Form):
@@ -19,3 +22,17 @@ class LoginForm(forms.Form):
                                        'placeholder': 'Password'
                                    }
                                ))
+
+    def clean(self):
+        username = self.cleaned_data['username']
+        if not User.objects.filter(username__iexact=username).exists():
+            raise ValidationError(
+                "Username/Password does not match"
+            )
+        user = User.objects.get(username__iexact=username)
+        password = self.cleaned_data['password']
+        if not user.check_password(raw_password=password):
+            raise ValidationError(
+                "Username/Password does not match"
+            )
+        return self.cleaned_data
--- mainapp/view/login.py
+++ mainapp/view/login.py
@@ -1,10 +1,8 @@
 from django.views.generic import TemplateView
 from mainapp.forms.login import LoginForm
 from django.shortcuts import render, redirect, reverse
-from django.http import JsonResponse
 from mainapp.models import User
-from django.contrib.auth.hashers import check_password
-from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth import login, logout
 
 
 class LoginView(TemplateView):
@@ -23,34 +21,16 @@ class LoginView(TemplateView):
 
     def post(self, request):
         login_form = LoginForm(request.POST)
-        if login_form.is_valid():
-            username = login_form.cleaned_data.get('username')
-            password = login_form.cleaned_data.get('password')
+        if not login_form.is_valid():
+            context = {
+                'login_form': login_form
+            }
 
-            user = User.fetch(username=username, is_superuser=None)
-            if not user:
-                return JsonResponse({
-                    "success": False,
-                    "message": "User not found."
+            return render(request, self.template_name, context=context)
 
-                })
-            if authenticate(request, username=username, password=password):
-                login(request, user)
-
-                return JsonResponse({
-                    'success': True
-                })
-
-            else:
-                return JsonResponse({
-                    "success": False,
-                    "message": "Wrong email or password."
-                })
-        else:
-            return JsonResponse({
-                "success": False,
-                "message": "Please Fill All the Fields."
-            })
+        user = User.objects.get(username__iexact=login_form.cleaned_data['username'])
+        login(request, user)
+        return redirect('mainapp:home')
 
 
 def logout_view(request):
