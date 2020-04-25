7ca3520c9f275612aaba423d1b6175c66dc1ba25
annshress <ann.shress@gmail.com>
2020-04-25 22:23:34 +0545

    fix: calendar

--- mainapp/view/calendars.py
+++ mainapp/view/calendars.py
@@ -1,7 +1,6 @@
 import json
 from datetime import datetime
 
-from django.db.models import Q
 from django.template.response import TemplateResponse
 
 from mainapp.models import PurchaseRecord
@@ -11,7 +10,8 @@ def calendar(request, year=None, month=None):
     now = datetime.now()
     year = year or now.year
     month = month or now.month
-    purchases = [
+
+    purchases_dc = [
         {
             "id": each.id,
             "customer_id": each.customer_id,
@@ -23,27 +23,42 @@ def calendar(request, year=None, month=None):
                 "place": each.customer.place or ""
             },
             "purchase": {
-                "watt": each.watt,
-                "count": each.module_count or "",
-                "price": each.price_without_tax or "",
-                "offer_date": str(each.offer_date) or "",
-                "reseller": each.reseller_name or "",
-                "dc": bool(each.dc_term),
+                # "watt": each.watt,
+                # "count": each.module_count or "",
+                # "price": each.price_without_tax or "",
+                # "offer_date": str(each.offer_date) or "",
+                # "reseller": each.reseller_name or "",
+                "dc": True,
                 "dc_term": str(each.dc_term) or "",
                 "dc_mechanic": each.dc_mechanic or "",
+            }
+        } for each in PurchaseRecord.objects.filter(
+            dc_term__month__gte=month-5,
+            dc_term__year__gte=year-1,
+        )
+    ]
+    purchases_ac = [
+        {
+            "id": each.id,
+            "customer_id": each.customer_id,
+            "name": each.customer.first_name + " " + each.customer.surname,
+            "dc_term": str(each.dc_term),
+            "customer": {
+                "street": each.customer.street or "",
+                "postcode": each.customer.postcode or "",
+                "place": each.customer.place or ""
+            },
+            "purchase": {
+                "dc": False,
                 "ac_term": str(each.ac_term) or "",
                 "ac_mechanic": each.ac_mechanic or "",
             }
         } for each in PurchaseRecord.objects.filter(
-            Q(
-                ac_term__month__gte=month - 2,
-                ac_term__year__gte=year - 1,
-            ) | Q(
-                dc_term__month__gte=month - 2,
-                dc_term__year__gte=year - 1,
-            )
-        )]
+            ac_term__month__gte=month-5,
+            ac_term__year__gte=year-1,
+        )
+    ]
     context = dict(
-        objects=json.dumps(purchases)
+        objects=json.dumps(purchases_dc + purchases_ac)
     )
     return TemplateResponse(request, 'calendars/calendar.html', context)
