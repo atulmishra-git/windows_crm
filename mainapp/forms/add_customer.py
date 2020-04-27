4e2e3bd2cab5321b7b4ce37820b2905480a7bb01
annshress <ann.shress@gmail.com>
2020-04-27 21:54:53 +0545

    fix customer form date

--- mainapp/forms/add_customer.py
+++ mainapp/forms/add_customer.py
@@ -12,3 +12,9 @@ class CustomerForm(LabelAdder, forms.ModelForm):
         widgets = {
             'birthday': forms.DateInput(attrs={'type': 'date'})
         }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # converting to string because form cannot handle date internationalization
+        if self.instance and self.instance.id:
+            self.initial['birthday'] = str(self.initial['birthday'])
