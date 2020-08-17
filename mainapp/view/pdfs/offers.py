fa959eb8beba2d8796c5f03ebe639add38957443
Atul Mishra <atulmishra-one@live.com>
2020-08-17 17:13:45 +0530

    pdf naming

--- mainapp/view/pdfs/offers.py
+++ mainapp/view/pdfs/offers.py
@@ -26,7 +26,7 @@ OPTIONS = {
 def save_to_attachment(customer, kind, rendered, by):
     dt = datetime.now()
     name = f"uploads/{customer.id}-{kind}-" \
-           f"{customer.first_name + '-' + customer.surname} " \
+           f"{customer.first_name + '-' + customer.surname}-" \
            f"{'%02d' % dt.day}-{'%02d' % dt.month}-{dt.year}.pdf"
     pdfkit.from_string(rendered, "media/" + name, options=OPTIONS)
     att = Attachments.objects.create(
